382ed7d4be6f7e2f9565ee6088c585bed41e2116
Tamara Trotter <tamaratrotter@Tamaras-iMac.local>
2019-10-21 05:14:41 -0400

    word wrap kata complete

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-module.exports = { 
-    Wrapper: function(text, columnNumber) {
-    return 'hello';
-    }
+module.exports = {
+	Wrapper: function wrap(text, columnNumber) {
+		if (text == null) return "";
+		//  Extracts ONLY the column number input from text entered ()
+		let wrappedLeft = text.substr(0, columnNumber).trim();
+		//  Begins the extraction at the position of the column number , then proceeds to extract the remainder of the string
+		let wrappedRight = text.substr(columnNumber).trim();
+
+		if (text.length <= columnNumber) return text;
+		if (text.length > columnNumber) {
+			wrappedLeft = wrap(wrappedLeft, columnNumber);
+			wrappedRight = wrap(wrappedRight, columnNumber);
+			//joins all perimeters and values defined
+			var wordWrapped = wrappedLeft.concat("\n").concat(wrappedRight);
+		}
+		return wordWrapped;
+	}
 };
--- test/appTest.js
+++ test/appTest.js
@@ -1,9 +1,24 @@
-const Wrapper = require('../app').Wrapper;
-const assert = require('chai').assert;
+const Wrapper = require("../app").Wrapper;
+const assert = require("chai").assert;
 // const app = require('../app');
 
-describe('App', function(){
-    it('app should return hello', function(){
-        assert.equal(Wrapper(), 'hello');
-    });
+describe("app", function() {
+	it(" should return '' ", function() {
+		assert.equal(Wrapper(null), "");
+	});
+	it(" should return T ", function() {
+		assert.equal(Wrapper("T", 1), "T");
+	});
+	it(" should return TT ", function() {
+		assert.equal(Wrapper("TT", 3), "TT");
+	});
+	it(" should return T\nT ", function() {
+		assert.equal(Wrapper("TT", 1), "T\nT");
+	});
+	it(" should return T\nT\nT ", function() {
+		assert.equal(Wrapper("TTT", 1), "T\nT\nT");
+	});
+	it(" should return T\nT ", function() {
+		assert.equal(Wrapper("T T", 1), "T\nT");
+	});
 });
